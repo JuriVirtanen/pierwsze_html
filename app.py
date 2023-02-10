03303271604b7d64e963bcc580648c5aba9d786d
Gabriel Marczak <121840299+JuriVirtanen@users.noreply.github.com>
2023-02-10 01:51:04 +0100

    Third try

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import *
-from flask import Flask # p1 dlaczego? czy * nie oznacza "wszystko"
+from flask import render_template, request, redirect
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/mypage/me")
 def me():
     return render_template("pierwsza.html")
 
-@app.route("/contact")
+@app.route("/mypage/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("druga.html")
-
-@app.route("/form")
-def form():
-    return render_template("formularz.html")
+    if request.method == 'GET':
+       return render_template("druga.html")
+    elif request.method == 'POST':
+       print(request.form)
+       return redirect("/mypage/me")
